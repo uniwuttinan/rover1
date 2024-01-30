6ee573e7ef966042f4f13a68c8ef3feb35a13459
uniwuttinan <117209788+uniwuttinan@users.noreply.github.com>
2024-01-30 11:58:01 +0700

    using tokio!

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,42 @@
-use std::sync::{Mutex, Arc};
-// use std::thread::{spawn};
-use rayon::prelude::*;
 
-fn main() {
-    println!("Hello, world! {}, {}", 1, "test");
+use std::sync::{Arc, Mutex};
+use tokio::time::sleep;
+use std::time::Duration;
+use futures::future;
 
-    let max = find_max(vec![1, 2, 3, 4, 5, 10]);
-    println!("max: {}", max);
+#[tokio::main]
+async fn main() {
+    let my_int = Arc::new(Mutex::new(0));
 
-    let my_int = Arc::new(Mutex::new(0i32));
+    // variable to store start time
+    let start = std::time::Instant::now();
 
-    // Create an array to store thread handles
-    // let mut handles = Vec::new();
+    let mut handles = Vec::new();
 
-    // Use rayon's parallel iterator to parallelize the loop
-    (0..10_000_000).into_par_iter().for_each(|_| {
-        modify_value(my_int.clone());
-    });
-
-    // Wait for all threads to finish
-    // for handle in handles {
-    //     handle.join().unwrap();
-    // }
-
-    // Access the modified string after the threads have finished
-    println!("new str: {}",  my_int.lock().unwrap());
-}
-
-fn find_max(nums: Vec<i32>) -> i32 {
-    let mut max: i32 = 0;
-    for n in nums {
-        if n > max {
-            max = n;
-        }
+    for _ in 0..10 {
+        let cloned = my_int.clone();
+        let handle = tokio::task::spawn(async move {
+            modify_value(cloned).await;
+        });
+        handles.push(handle);
     }
-    max
+
+    future::join_all(handles).await;
+
+    println!("Final value = {}", *my_int.lock().unwrap());
+
+    // variable to store end time
+    let end = std::time::Instant::now();
+
+    // print time difference in milliseconds
+    println!("Time used: {}ms", (end - start).as_millis());
+
 }
 
-fn modify_value(inv: Arc<Mutex<i32>>) {
-    let mut locked = inv.lock().unwrap();
-    *locked += 1;
+async fn modify_value(inv: Arc<Mutex<i32>>) {
+    {
+        let mut locked = inv.lock().unwrap();
+        *locked += 1;
+    }
+    sleep(Duration::from_secs(1)).await;
 }
